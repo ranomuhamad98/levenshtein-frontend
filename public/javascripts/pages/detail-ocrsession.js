31d8aca17b279e842caa4ca5dcb3f8d004aff1b1
M. Huda <mhuda@Ms-MacBook-Pro.local>
2022-09-06 17:40:59 +0700

    format date

--- public/javascripts/pages/detail-ocrsession.js
+++ public/javascripts/pages/detail-ocrsession.js
@@ -18,6 +18,9 @@ export class DetailOcrSessionPage
     {
         me.getSession(me.id, { success: function(session){
             me.ocrSession = session;
+            me.ocrSession.sessionStartDate = moment(me.ocrSession.sessionStartDate).format('DD-MM-YYYY hh:mm:ss')
+            me.ocrSession.sessionEndDate = moment(me.ocrSession.sessionEndDate).format('DD-MM-YYYY hh:mm:ss')
+
 
             let ocrResults = me.ocrSession.ocrResult;
             //console.log(ocrResults)
