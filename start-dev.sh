0364963e57863ae9d4b07c4da4dde30cabfa630d
M. Huda <mhuda@Ms-MacBook-Pro.local>
2022-09-30 11:14:58 +0700

    adding auto view

---
+++ start-dev.sh
@@ -0,0 +1,4 @@
+export GOOGLE_APPLICATION_CREDENTIALS=/Users/mhuda/Works/Credentials/levenshtein-dev-app.json
+rm .env
+cp .env-dev .env
+node app.js
---
+++ start-spindo.sh
@@ -0,0 +1,4 @@
+export GOOGLE_APPLICATION_CREDENTIALS=/Users/mhuda/Works/Credentials/lv-tennant-spindo-owner.json
+rm .env
+cp .env-spindo .env
+node app.js
